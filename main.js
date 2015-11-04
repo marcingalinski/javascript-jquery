2e630c19f4d7b4d709e55984e81827121af15f81
Marcin Galiński <marcin.galinski95@gmail.com>
2015-11-04 15:12:42 +0100

    add button to clear and resize grid

--- main.js
+++ main.js
@@ -1,15 +1,32 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
+var size = 16;
+
+var paint = function () {
+    $('td').on('mouseenter', function () {
+        $(this).addClass('hovered');
+    } );
+};
+
+var makeGrid = function ( rows, cols) {
+    $('table').children().remove();
+    for (i = 1; i <= rows; i++) {
+        jQuery('table').append('<tr></tr>');
+    }
+    for (i = 1; i <= cols; i++) {
+        jQuery('tr').append('<td></td>');
+    }
+    paint();
+};
 
 $(document).ready(function() {
     $('#container').append('<table></table>');
-    var makeGrid = function ( rows, cols) {
-        for (i = 1; i <= rows; i++) {
-            jQuery('table').append('<tr></tr>');
-        }
-        for (i = 1; i <= cols; i++) {
-            jQuery('tr').append('<td></td>');
-        }
-    };
-    makeGrid(16, 16);
+    makeGrid(size, size);
+} );
+
+$(document).ready(function() {
+    $('button').on('click', function () {
+        var size = prompt('size of a grid', size);
+        makeGrid(size, size);
+    } );
 } );
